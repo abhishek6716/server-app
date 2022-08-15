1235bca21cd4fc935ef691338ee4c1653b20d516
abhishek6716 <abhisheksingh6716@gmail.com>
2022-08-15 10:33:43 +0530

    event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,23 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'testServer', content: 'testContent' },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBluePrintAdded(bluePrintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent,
+    });
+  }
 }
